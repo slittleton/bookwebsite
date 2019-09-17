5dc6705bcc80653e2c9e6c512f81fc1ec707fce7
slittleton <littletonltp@gmail.com>
2019-09-16 17:11:43 -0700

    home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import "../css/home.css";
+
+import bordertop from "../assets/img/border-main-top-4.png";
+import borderbottom from "../assets/img/border-main-bottom-2.png";
+import cover from "../assets/img/boogey-cov.png";
+import back from "../assets/img/boogey-back.png";
 
 const Home = React.forwardRef((props, homeRef) => {
-  return(
-    <div className="home"  ref={homeRef}>
-      Home
+  return (
+    <div className="home" ref={homeRef}>
+      <div className="banner-container">
+        <div className="title-main">The Boogeyman Invesitagtion</div>
+      </div>
+
+      <div className="main-img">
+        <div className="border">
+          <img src={bordertop} alt="" className="banner-decor" />
+        </div>
+
+        <div className="covers">
+        <img src={cover} alt="" className="cover" />
+        <img src={back} alt="" className="cover" />
+
+        </div>
+
+        <div className="border">
+          <img src={borderbottom} alt="" className="banner-decor" />
+        </div>
+      </div>
     </div>
-  )
-})
+  );
+});
 
 export default Home;
