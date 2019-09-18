66cfc61e353c3d11c2f043b24103c35121f5cf9c
slittleton <littletonltp@gmail.com>
2019-09-18 15:51:49 -0700

    gallery

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/gallery.css";
 
 const API = process.env.REACT_APP_API_URL;
-const pageNum = 1;
+
+let pageNums = Array.from(Array(38).keys());
 
 const Gallery = React.forwardRef((props, galleryRef) => {
+  const [pageNum, setPageNum] = useState(0);
+
   return (
     <div className="gallery" ref={galleryRef}>
       <div className="title-container">
@@ -12,16 +15,38 @@ const Gallery = React.forwardRef((props, galleryRef) => {
       </div>
       <div className="text-container">
         <div className="text">
-          You can read the entire book here or download it to check it out offline.
+          You can read the entire book here or download it to check it out
+          offline.
         </div>
       </div>
 
       <div className="main-img">
-        <img src={`${API}/pages/${pageNum}.jpg`} alt="page"/>
+        <img src={`${API}/pages/${pageNum}.jpg`} alt="page" />
       </div>
 
-
-
+      <div className="img-nav-container">
+        <div className="img-nav">
+          <div className="img-grid">
+            {pageNums.map(num => {
+              return (
+                <div
+                  className="page"
+                  onClick={() => {
+                    setPageNum(num);
+                  }}
+                >
+                  
+                  <img
+                    src={`${API}/pages/${num}.jpg`}
+                    alt="page"
+                    className="thumb"
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 });
